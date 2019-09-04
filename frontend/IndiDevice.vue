89c893b7da56df6d8d525343803f24b3d830b2d4
Joseph Long <github@joseph-long.com>
2019-09-04 01:36:56 -0700

    Get INDI properties deffing and setting and deleting on the fly

---
+++ frontend/IndiDevice.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <div v-for="(prop, propName) in device">
+      {{ prop._kind }}
+      <div v-if="prop._kind == 'SWITCH'">switch component here</div>
+      <div v-if="prop._kind == 'NUMBER'">number component here</div>
+      <div v-if="prop._kind == 'LIGHT'">light component here</div>
+      <div v-if="prop._kind == 'TEXT'">text component here</div>
+      <div v-for="(elem, elemName) in prop.elements">
+        <div>
+          {{ prop.state }} {{ prop.group }} {{ prop.label }} {{ name }}.{{ propName }}.{{ elemName }}={{ elem.value }} {{ prop.perm }}
+          <input v-if="prop.perm == 'rw'" :placeholder="elem.value">
+        </div>
+      </div>
+    </div>
+    <button @click="$emit('deviceChange')">emit deviceChange</button>
+  </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+  props: ["name", "device"]
+}
+</script>
