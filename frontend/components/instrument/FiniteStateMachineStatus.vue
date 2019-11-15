ceb0b8f5053a5358d2c7baaaf7fedc17d86db4a5
Joseph Long <github@joseph-long.com>
2019-11-15 18:27:56 -0300

    Add FiniteStateMachineStatus component

---
+++ frontend/components/instrument/FiniteStateMachineStatus.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="fsm" :class="fsmState.toLowerCase()">{{ fsmState }}</div>
+</template>
+<style lang="scss" scoped>
+@import "./css/variables.scss";
+
+.failure, .error, .nodevice, .poweroff {
+  color: $error;
+}
+.poweron, .notconnected, .connected, .loggedin, .configuring, .nothomed, .homing {
+  color: $alert;
+}
+.ready, .operating {
+  color: $ok;
+}
+.shutdown {
+  color: $error;
+}
+</style>
+<script>
+import indi from "~/mixins/indi.js";
+// FAILURE=-20,       ///< The application has failed, should be used when m_shutdown is set for an error.
+// ERROR=-10,         ///< The application has encountered an error, from which it is recovering (with or without intervention)
+// UNINITIALIZED = 0, ///< The application is unitialized, the default
+// INITIALIZED = 1,   ///< The application has been initialized, set just before calling appStartup().
+// NODEVICE = 2,      ///< No device exists for the application to control.
+// POWEROFF = 4,      ///< The device power is off.
+// POWERON = 6,       ///< The device power is on.
+// NOTCONNECTED = 8,  ///< The application is not connected to the device or service.
+// CONNECTED = 10,     ///< The application has connected to the device or service.
+// LOGGEDIN = 15,      ///< The application has logged into the device or service
+// CONFIGURING = 20,   ///< The application is configuring the device.
+// NOTHOMED = 24,     ///< The device has not been homed.
+// HOMING = 25,       ///< The device is homing.
+// OPERATING = 30,    ///< The device is operating, other than homing.
+// READY = 35,        ///< The device is ready for operation, but is not operating.
+// SHUTDOWN = 10000   ///< The application has shutdown, set just after calling appShutdown().
+
+export default {
+  props: ["device", "indiId"],
+  mixins: [indi],
+  computed: {
+    fsmState() {
+      if (!this.thisDevice) return "waiting";
+      return this.thisDevice.properties["fsm"].elements["state"].value;
+    }
+  }
+}
+</script>
