6af905d841a79bb1ca77c82a6f0904ca41f3e2bc
Joseph Long <github@joseph-long.com>
2023-03-06 00:46:13 +0000

    Add observer saving monitor component

---
+++ frontend/components/instrument/ObserverSavingMonitor.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="observer-saving-monitor" :class="{'active': isSaving}">
+    <span class="save-path">
+      /data/obs/{{ currentObserverEmail }}/{{ semester }}/{{ dateNight }}/<indi-value indi-id="tcsi.catalog.object"></indi-value>/<indi-value indi-id="observers.obs_name.current"></indi-value>_{{ maybeDateStamp }}/{{ enabledCamerasShellSnippet }}
+    </span>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import "./css/variables.scss";
+.observer-saving-monitor { 
+    background: var(--bg-alternate);
+    transition: background 1s;
+    &.active {
+        background: $plasma-blue;
+        color: var(--fg-normal);
+    }
+}
+.save-path {
+  font-family: monospace;
+}
+</style>
+<script>
+import indi from "~/mixins/indi.js";
+import utils from "~/mixins/utils.js";
+import IndiValue from "~/components/indi/IndiValue.vue";
+import IndiSwitchMultiElement from "~/components/indi/IndiSwitchMultiElement.vue";
+import IndiSwitchDropdown from "../indi/IndiSwitchDropdown.vue";
+import IndiToggleSwitch from "../indi/IndiToggleSwitch.vue";
+import AlternateIndiToggleSwitch from "../indi/AlternateIndiToggleSwitch.vue";
+import IndiElement from "../indi/IndiElement.vue";
+import IndiProperty from "~/components/indi/IndiProperty.vue";
+
+export default {
+  props: {
+    device: {
+      type: String,
+      default: "observers",
+    },
+    camNames: {
+      type: Array,
+      default: () => ["sci1", "sci2", "wfs", "lowfs", "tip", "acq"],
+    },
+  },
+  mixins: [indi, utils],
+  components: {
+    IndiValue,
+    IndiSwitchMultiElement,
+    IndiSwitchDropdown,
+    IndiToggleSwitch,
+    IndiElement,
+    IndiProperty,
+    AlternateIndiToggleSwitch
+  },
+  computed: {
+    currentObserverEmail() {
+      let prop = this.retrieveByIndiId("observers.observers");
+      if (!prop) return;
+      console.log(prop._elements);
+        for (let elementKey of Object.keys(prop._elements)) {
+          if (prop._elements[elementKey]._value == 'On') {
+            return prop._elements[elementKey].label;
+          }
+      }
+    },
+    isSaving() {
+      return (this.enabledCameras.length > 0) && (this.retrieveByIndiId("observers.obs_on.toggle")?._value == 'On');
+    },
+    enabledCameras() {
+      let currentCameras = [];
+      for (let camName of this.camNames) {
+        const isEnabled = this.retrieveValueByIndiId(`observers.writers.cam${camName}`) == 'On';
+        if (isEnabled) {
+            currentCameras.push(`cam${camName}`);
+        }
+        
+      }
+      return currentCameras;
+    },
+    enabledCamerasShellSnippet() {
+        if (this.enabledCameras.length == 0) return '';
+        let snippet = '{';
+        for (let fullCamName of this.enabledCameras) {
+            snippet += fullCamName + ',';
+        }
+        
+        if (this.enabledCameras.length == 1) { 
+            snippet = snippet.slice(1, -1);
+        } else {
+            snippet = snippet.slice(0, -1) + '}';
+        }
+        return snippet;
+    },
+    semester() {
+      return this.scheduleSemester(this.time.currentTime);
+    },
+    dateNight() {
+      return this.dateNightFromDateTime(this.time.currentTime);
+    },
+    catalogObject() {
+      let elem = this.retrieveByIndiId("tcsi.catalog.object");
+      if (!elem) return null;
+      return elem._value;
+    },
+    dateStamp() {
+      return this.time.currentTime.toFormat("yyyyMMdd'T'HHmmss");
+    },
+    maybeDateStamp() {
+        return this.isSaving ? '*' : this.dateStamp;
+    }
+  },
+};
+</script>
