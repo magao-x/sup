1b8efc4737508ff0f96a88d90bfcd44162fa8860
Irina Stefan <istefan@arizona.edu>
2024-08-08 18:00:05 -0700

    Added error message in frontend/components/basic/TabBar.vue if the 'layout' value is invalid.

--- frontend/components/basic/TabBar.vue
+++ frontend/components/basic/TabBar.vue
@@ -1,8 +1,15 @@
 <template>
   <TabBarScoob v-if="$store.state.config.layout === 'SCOOB'"></TabBarScoob>
   <TabBarMagAOX v-else-if="$store.state.config.layout === 'MAGAOX'"></TabBarMagAOX>
-  <TabBarMagAOX v-else></TabBarMagAOX>
+  <div v-else class="error"><span>Configuration 'layout' value '{{ $store.state.config.layout }}'' is invalid. Please update the config file.</span></div>
 </template>
+<style lang="scss" scoped>
+@import "~/css/variables.scss";
+.error {
+  padding: 1em;
+  color: $error;
+}
+</style>
 <script>
 import constants from "~/constants.js";
 import TabBarMagAOX from "~/pages/MagAOX/TabBar.vue";
